9c8d7ed23702193675e80d82fe32374e98044d75
Haison1111 <blowup441@gmail.com>
2023-05-15 22:11:33 +0700

    nop bai

--- bai2.js
+++ bai2.js
@@ -1,11 +1,14 @@
 function numberOneTriangle(n){
-    
-    for(let i = 0; i<n;i++){
+    if ( n<=10 && n > 0){
+    for(let i = 0; i < n;i++){
         let tamgiac = ""
         for ( let j = 0; j <= i;j++){
            tamgiac += "*"
         }
         console.log(tamgiac)
     }
+} else{
+    return false;
+}
 }
 numberOneTriangle(4)
